53a31adee48756eb36171618c9f138deb6c4cb68
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-04-12 23:56:28 +0200

    before removing MniTube Video class

--- main.rb
+++ main.rb
@@ -5,76 +5,6 @@ require 'korundum4'
 
 require 'phonon'
 
-=begin
-Dragon::VolumeAction::VolumeAction( QObject *receiver, const char *slot, KActionCollection *ac )
-        : KToggleAction( i18nc( "Volume of sound output", "Volume"), ac )
-{
-    setObjectName( "volume" );
-    setIcon( KIcon( "player-volume" ) );
-    setShortcut( Qt::Key_V );
-    ac->addAction( objectName(), this );
-    connect( this, SIGNAL( triggered( bool ) ), receiver, slot );
-    connect( engine(), SIGNAL( mutedChanged( bool ) ), this, SLOT( mutedChanged( bool ) ) );
-}
-
-void
-Dragon::VolumeAction::mutedChanged( bool mute )
-{
-    if( mute )
-        setIcon( KIcon( "player-volume-muted" ) );
-    else
-        setIcon( KIcon( "player-volume" ) );
-}
-=end
-
-module KDE
-  class ToogleAction < KDE::Action
-  end
-end
-
-module MT
-  class VolumeAction < KDE::ToogleAction
-
-    slots 'muted_changed(bool)'
-
-    def initialize *args
-      super
-
-      set_object_name 'volume'
-      set_icon KDE::Icon.new 'player-volume'
-      set_shortcut KDE::Shortcut.new Qt::Key_V
-    end
-
-    def muted_changed mute
-      set_icon KDE::Icon.new mute ? 'player-volume-muted' : 'player-volume'
-    end
-
-  end
-
-=begin
-    muteAction = new KAction(i18nc("'Audio' menu", "Mute Volume"), this);
-    mutedIcon = KIcon("audio-volume-muted");
-    unmutedIcon = KIcon("audio-volume-medium");
-    muteAction->setIcon(unmutedIcon);
-    muteAction->setShortcut(KShortcut(Qt::Key_M, Qt::Key_VolumeMute));
-=end
-#   class MuteAction < KDE::ToogleAction
-#     slots 'is_muted(bool)'
-#
-#     def initialize *args
-#       super *args
-#
-#       set_object_name 'mutedAction'
-#       set_shortcut KDE::Shortcut.new Qt::Key_M, Qt::Key_VolumeMute
-#       set_text = i18n 'Mute Volume'
-#       connect SIGNAL :triggered, self, SLOT
-#
-#     def is_muted muted
-#
-#     end
-#   end
-end
-
 class Video < Qt::Object
   attr_accessor :author, :title, :description, :webpage, :duration, :views, \
                 :published_datetime
@@ -109,7 +39,7 @@ class Video < Qt::Object
   def set_thumbnail str # slot
   end
 
-  private
+#   private
 
   def got_video_info str # slot
   end
@@ -125,35 +55,7 @@ class Video < Qt::Object
 
 end
 
-class VideoMimeData < Qt::MimeData
-  attr_reader :videos
 
-  def initialize *args
-    super *args
-
-    @videos = Array.new # expects Video elements
-  end
-
-  def push_video video
-    @videos.push video
-  end
-
-  MIME_TYPE = 'application/x-minitube-video'
-
-  def has_video_mimetype? mimeType
-    mimeType == MIME_TYPE
-  end
-
-#   alias formats_QMimeData formats
-
-  def formats
-#     formats = formats_QMimeData # is QStringList
-#     formats.push MIME_TYPE
-    [MIME_TYPE]
-  end
-
-
-end
 class CustomWidget < KDE::MainWindow
 
   slots 'toogleVolumeSlider(bool)', 'setFullScreen(bool)', \
@@ -171,50 +73,89 @@ class CustomWidget < KDE::MainWindow
   end
 
   def stateChanged state, stateBefore
-    if state == Phonon::PlayingState
-      @playPauseAction.checked = true
-    elsif state == Phonon::PausedState
-      @playPauseAction.checked = false
-    else
-      #...
-    end
+#     if state == Phonon::PlayingState
+#       action.checked = true
+#     elsif state == Phonon::PausedState
+#       action.checked = false
+#     else
+#       #...
+#     end
   end
 
-  def ini_phonon
+  def ini_phonon collection, menu, controlBar
     @videoPlayer = Phonon::VideoPlayer.new Phonon::VideoCategory, self
+    volumeSlider = Phonon::VolumeSlider.new @videoPlayer.audioOutput, self
+    seekSlider = Phonon::SeekSlider.new @videoPlayer.mediaObject, self
 
-    @seekSlider = Phonon::SeekSlider.new @videoPlayer.mediaObject, self
-
-    @volumeSlider = Phonon::VolumeSlider.new @videoPlayer.audioOutput, self
-
-    @muteAction = @actionCollection.addAction 'volume-mute', KDE::Action.new( KDE::Icon.new( 'audio-volume-medium' ), i18n( 'Mute Volume' ), self)
-    @muteAction.checkable = true
-    @muteAction.shortcut = KDE::Shortcut.new Qt::Key_M, Qt::Key_VolumeMute
-    @muteAction.connect( SIGNAL('toggled(bool)') ) do |muted|
-      @muteAction.set_icon KDE::Icon.new muted ? 'player-volume' : 'player-volume-muted' # audio-volume-muted' : 'audio-volume-medium'
-      @videoPlayer.audioOutput.muted = muted
-    end
-    connect(@volumeSlider.audioOutput, SIGNAL('mutedChanged(bool)'), @muteAction, SLOT('setChecked(bool)') )
-
-
-    @playPauseAction = @actionCollection.addAction 'switch-pause', KDE::Action.new( self )
-    @playPauseAction.checkable = true
-    @playPauseAction.shortcut = KDE::Shortcut.new( Qt::Key_Backspace, Qt::Key_MediaStop )
-    @playPauseAction.connect( SIGNAL('toggled(bool)') ) do |playing|
+    # action play pause
+    action = collection.add_action 'switch-pause', KDE::Action.new( self )
+    action.checkable = true
+    action.shortcut = KDE::Shortcut.new Qt::Key_Backspace, Qt::Key_MediaStop
+    action.connect( SIGNAL('toggled(bool)') ) do |playing|
       if playing
         @playPauseAction.text = i18n '&Play'
-        @playPauseAction.icon = KDE::Icon.new 'media-playback-start'
+        @playPauseAction.icon = KDE::Icon.new 'media-playback-pause'
         @videoPlayer.play
       else
         @playPauseAction.text = i18n '&Pause'
-        @playPauseAction.icon = KDE::Icon.new 'media-playback-pause'
+        @playPauseAction.icon = KDE::Icon.new 'media-playback-start'
         @videoPlayer.pause
       end
     end
     connect(@videoPlayer.mediaObject, SIGNAL('stateChanged(Phonon::State, Phonon::State)'), self, SLOT('stateChanged(Phonon::State, Phonon::State)'))
-#     @videoPlayer.mediaObject.connect( SIGNAL('stateChanged(Phonon::State, Phonon::State)') ) do
-#       puts 'hier'
-#     end
+    menu.add_action action
+    controlBar.add_action action
+
+    # action previous
+    action = collection.add_action 'controls-previous', KDE::Action.new( KDE::Icon.new( 'media-skip-backward' ), i18n( 'Previous' ), self )
+    action.shortcut = KDE::Shortcut.new Qt::Key_PageUp, Qt::Key_MediaPrevious
+    menu.add_action action
+    controlBar.add_action action
+
+    # action stop
+    action = collection.add_action 'controls-stop', KDE::Action.new( KDE::Icon.new( 'media-playback-stop' ), i18n( 'Stop' ), self )
+    action.shortcut = KDE::Shortcut.new Qt::Key_Backspace, Qt::Key_MediaStop
+    menu.add_action action
+    controlBar.add_action action
+
+    # action forward
+    action = collection.add_action 'controls-forward', KDE::Action.new( KDE::Icon.new( 'media-skip-forward' ), i18n( 'Forward' ), self )
+    action.shortcut = KDE::Shortcut.new Qt::Key_PageDown, Qt::Key_MediaNext
+    menu.add_action action
+    controlBar.add_action action
+
+    menu.add_separator
+
+    # action volume mute
+    audioMenu = KDE::Menu.new i18nc( 'Playback menu', 'Audio' ), self
+    menu.add_menu audioMenu
+
+    action = collection.add_action 'volume-mute', KDE::Action.new( KDE::Icon.new( 'player-volume' ), i18n( 'Mute Volume' ), self)
+    action.checkable = true
+    action.shortcut = KDE::Shortcut.new Qt::Key_M, Qt::Key_VolumeMute
+    action.connect( SIGNAL('toggled(bool)') ) do |muted|
+      action.set_icon KDE::Icon.new muted ? 'player-volume-muted' : 'player-volume' # audio-volume-muted' : 'audio-volume-medium'
+      @videoPlayer.audioOutput.muted = muted
+    end
+    connect(volumeSlider.audioOutput, SIGNAL('mutedChanged(bool)'), action, SLOT('setChecked(bool)') )
+    audioMenu.add_action action
+
+    menu.add_separator
+
+    # action fullscreen
+#     action = collection.add_action 'switch-fullscreen', KDE::StandardAction::fullScreen( @videoPlayer.video_widget, SLOT( 'setFullScreen(bool)' ), self, collection)
+    action = collection.add_action 'switch-fullscreen', KDE::StandardAction::fullScreen( self, SLOT( 'setFullScreen(bool)' ), self, collection)
+    menu.add_action action
+    controlBar.add_action action
+
+    action = collection.add_action 'volume-slider', KDE::Action.new( i18n( 'Volume Slider' ), self )
+    action.default_widget = volumeSlider
+    controlBar.add_action action
+
+    action = collection.add_action 'seek-slider', KDE::Action.new( i18n( 'Position Slider' ), self )
+    action.default_widget = seekSlider
+    controlBar.add_action action
+
   end
 
   def initialize
@@ -231,132 +172,54 @@ class CustomWidget < KDE::MainWindow
 #     @config.sync
 
 
-    ##### Prepare Central Widget
-#     @centralwidget = KDE::TextEdit.new self
-    @actionCollection = KDE::ActionCollection.new self
-    ini_phonon
-    setCentralWidget @videoPlayer
-
-#     @mediaObject = Phonon::MediaObject.new self
-#     @mediaObject.setCurrentSource Phonon::MediaSource.new('/home/rriemann/Documents/Videos/Sita_Sings_the_Blues.ogv')
-#     @centralwidget.play @mediaObject
-#     @videoPlayer.play Phonon::MediaSource.new('/home/rriemann/Documents/Videos/Sita_Sings_the_Blues.ogv')
-    @videoPlayer.play Phonon::MediaSource.new('/home/rriemann/Documents/Videos/Player/Austin_Powers_Goldstaender_08.08.15_20-15_rtl2_115_TVOON_DE.mpg.mp4')
-#       @centralwidget.play Phonon::MediaSource.new('http://videos.mozilla.org/firefox/3.6/meetfirefox/FF3.6_Screencast_FINAL.ogv')
-
     #### prepare menus
+    collection = KDE::ActionCollection.new self
+    controlBar = KDE::ToolBar.new 'control_bar', self, Qt::BottomToolBarArea
+    controlBar.tool_button_style = Qt::ToolButtonIconOnly
 
     menu = KDE::Menu.new i18n('&File'), self
-    menuBar.addMenu menu
-    playerMenu = KDE::Menu.new i18n('&Play'), self
-    menuBar.addMenu playerMenu
-    controlBar = KDE::ToolBar.new 'control_bar', self, Qt::BottomToolBarArea
+    menuBar.add_menu menu
 
-    ##### Prepare the Actions
+    action = collection.add_action 'quit', KDE::StandardAction::quit( self, SLOT( :close ), collection )
+    menu.add_action action
+    controlBar.add_action action
 
-#     volumeAction = MT::VolumeAction.new i18nc( 'Volume of sound output', 'Volume' ), @actionCollection
-#     addAction volumeAction
+    menu = KDE::Menu.new i18n('&Play'), self
+    menuBar.add_menu menu
 
-    closeAction = KDE::StandardAction::quit( self, SLOT( :close ), @actionCollection )
-    menu.addAction closeAction
-    controlBar.addAction closeAction
+    ini_phonon collection, menu, controlBar
 
-    actionPrevious = KDE::Action.new KDE::Icon.new( 'media-skip-backward' ), i18n( 'Previous' ), self
-    actionPrevious.set_shortcut KDE::Shortcut.new Qt::Key_PageUp, Qt::Key_MediaPrevious
-    @actionCollection.add_action 'controls_previous', actionPrevious
-    playerMenu.add_action actionPrevious
-    controlBar.add_action actionPrevious
+    menu = KDE::Menu.new i18n('&Settings'), self
+    menuBar.add_menu menu
 
-    playerMenu.add_action @actionPlayPause
-    controlBar.add_action @actionPlayPause
+    action = collection.add_action 'configure-keys', KDE::StandardAction::keyBindings( self, SLOT( :configureKeys ), collection )
+    menu.add_action action
 
-    actionStop = KDE::Action.new KDE::Icon.new( 'media-playback-stop' ), i18n( 'Stop' ), self
-    actionStop.set_shortcut KDE::Shortcut.new Qt::Key_Backspace, Qt::Key_MediaStop
-    @actionCollection.add_action 'controls_stop', actionStop
-    playerMenu.add_action actionStop
-    controlBar.add_action actionStop
+    menuBar.add_menu helpMenu
 
-    actionForward = KDE::Action.new KDE::Icon.new( 'media-skip-forward' ), i18n( 'Forward' ), self
-    actionForward.set_shortcut KDE::Shortcut.new Qt::Key_PageUp, Qt::Key_MediaPrevious
-    @actionCollection.add_action 'controls_forward', actionForward
-    playerMenu.add_action actionForward
-    controlBar.add_action actionForward
-
-    playerMenu.add_separator
-
-    actionFullScreen = KDE::Action.new KDE::Icon.new( 'view-fullscreen' ), i18n( 'Full Screen Mode' ), self
-    actionFullScreen.set_shortcut KDE::Shortcut.new Qt::Key_F
-    @actionCollection.add_action 'view_fullscren', actionFullScreen
-    playerMenu.add_action actionFullScreen
-#     controlBar.add_action actionFullScreen
-
-    playerMenu.add_separator
-
-    audioMenu = KDE::Menu.new i18nc( 'Playback menu', 'Audio' ), self
-    playerMenu.add_menu audioMenu
-
-
-    audioMenu.add_action @muteAction
-#     controlBar.add_action @muteAction
-
-    volumeSliderAction = KDE::Action.new i18n( 'Volume Slider' ), self
-    volumeSliderAction.default_widget = @volumeSlider
-    @actionCollection.add_action 'controls_volume_slider', volumeSliderAction
-    controlBar.add_action volumeSliderAction
-
-    positionSlider = KDE::Action.new i18n( 'Position Slider' ), self
-    positionSlider.default_widget = @seekSlider
-    @actionCollection.add_action 'controls_position_slider', positionSlider
-    controlBar.add_action positionSlider
-
-
-
-
-    @actionCollection.associate_widget self
-    @actionCollection.read_settings
+    collection.associate_widget self
+    collection.read_settings
     set_auto_save_settings
 
     menuBar.show
     controlBar.show
 
+    setCentralWidget @videoPlayer
+    youtube_url = 'http://www.youtube.com/watch?v=lg8LfoyDFUM'
+    a = Time.now
+    title,video_url = `python youtube-dl -geb #{youtube_url}`.strip.split $/
+    puts (Time.now - a)
+    setWindowTitle title
+    @videoPlayer.play Phonon::MediaSource.new video_url
 
-    controlBar.set_tool_button_style Qt::ToolButtonIconOnly
-
-
-#     @actionQuit = KDE::Action.new( self ) {
-#       setIcon KDE::Icon.new "application-exit"
-#     }
-#     connect( @actionQuit, SIGNAL( :triggered ), SLOT( :close ) )
-
-#     ##### Prepare the Menu
-#     @menuBar = menuBar
-#     @menuFile = Qt::Menu.new @menuBar
-#     @menuFile.addAction @actionQuit
-#     @menuBar.addAction @menuFile.menuAction
-#     @helpMenu = helpMenu
-#     @menuBar.addAction @helpMenu.menuAction
-#
-#     setMenuBar(@menuBar)
-#
-#     ##### Prepare Statusbar
-#     @statusBar = statusBar
-#     setStatusBar @statusBar
-#
-    ##### Prepare Toolbar
-#     @toolBar = toolBar
-#     @toolBar.addAction @actionQuit
-#     addToolBar(Qt::TopToolBarArea, @toolBar)
-
-
-
-    retranslateUi
+#     retranslateUi
 
     self.show
   end
 
   def retranslateUi
 #     @menuFile.title = i18n "File"
-    setWindowTitle i18n "MainWindow"
+#     setWindowTitle i18n "MainWindow"
     # @statusBar.showMessage i18n "Loading"
 #     @actionQuit.text = i18n "Quit"
 #     @actionQuit.shortcut =  KDE::Shortcut.new i18nc( "Quit", "Ctrl+Q" )
